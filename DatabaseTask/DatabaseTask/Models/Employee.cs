6af01b3e044b306cba1e817b8f29216abb00ebcd
mur3l <tippitauno@gmail.com>
2025-06-12 13:57:46 +0300

    Tihend

--- DatabaseTask/DatabaseTask/Models/Employee.cs
+++ DatabaseTask/DatabaseTask/Models/Employee.cs
@@ -1,6 +1,19 @@
-﻿namespace DatabaseTask.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DatabaseTask.Core.Domain
 {
     public class Employee
     {
-    }
-}
+        [Key]
+        public int Id { get; set; }
+        public string Department { get; set; }
+        public string IntranetId { get; set; }
+        public string Child { get; set; }
+        public string LoanableItem { get; set; }
+        public string Vacation { get; set; }
+        public string Hint { get; set; }
+        public string SickLeave { get; set; }
+        public string HRRequest { get; set; }
+        public string HealthCheck { get; set; }
+
